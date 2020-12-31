bcfa5a7f640645e7cac601efff5bda5ed780e9ce
PonykillerMX <emilioanderevalencia@gmail.com>
2020-12-31 12:06:44 -0500

    adding all the files

---
+++ connectToCurrencies.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class currencyScraper():
+    """ This class focuses on scraping coindesk.com"""
+            
+    def __init__(self, coin):
+        # Define the self functions. 
+        self.coin = coin
+        self.url = f"https://www.coindesk.com/price/{coin}"
+        
+        self.webpage = requests.get(self.url)
+
+        self.soup = BeautifulSoup(self.webpage.content, "html.parser")
+        
+        # Get coin price
+        first_results = str(self.soup.find_all("div", class_="price-large"))  
+
+        second_results = first_results.split("span>") 
+
+        third_results = str(second_results[-1]).split("</div>")
+        
+        self.price = float(third_results[0].replace(",", ""))
+
+    
+    def __str__(self): 
+        """When user prints(coin), return this"""
+        return f"\nCoin: {self.coin} | Price: {self.price} | URL: {self.url}\n"
+    
+    def getName(self): 
+        """Just get the name of the coin you're using"""
+        return f"{self.coin}"
+
+    def getPrice(self):
+        """Get the current price of the chosen coin"""
+        return f"{self.coin} price: {self.price}"
+    
+    def getURL(self): 
+        """Get the URL to gain more complete info"""
+        return self.url
+
+    def getInfo(self): 
+        """Get a paragraph with coin information. 
+        Doesn't work that well becuase not every coin has an explanation 
+        and if they do, they're structured very differently in HTML"""
+        first_results = str(self.soup.find_all("div", class_="coin-about-text"))
+
+        self.info = str(first_results).strip()
+        
+        return self.info
+    
+    def set_min_and_max(self): 
+        """Set the max and the min for the coin"""
+        self.max = float(input(f"Max price for {self.getName()} => "))
+        self.min = float(input(f"Min price for {self.getName()} => ")) 
+        # Check if max and min are in the correct intervals. 
+        if self.max < self.price and self.min > self.price: 
+            return "ERROR: Max less than current price or min more than current price"
+        # If it is set max price and min price and return the values set.  
+        elif self.max > self.price and self.min < self.price: 
+            return f"Max price: {self.max} | Min price: {self.min}" 
+
+
---
+++ main.py
@@ -0,0 +1,90 @@
+#!usr/bin/python3
+import sys  # To exit program
+from connectToCurrencies import currencyScraper  # Module with class 
+
+
+options = """
+\nOptions include but are not limited to bitcoin, xrp, ethereum,
+stellar, chainlink, tether, litecoin, cardano, cosmos, etc... 
+
+but remember you can try any coin you wish that's available in coindesk.\n"""
+
+
+def choose_coin_to_manage(): 
+    #Dedicated to collect input from user, what coin they want to manage
+    chosen_coin = input("\n | choose crypto | (e) examples | (q) quit |\n\n ===> ").lower().strip() 
+    return chosen_coin 
+
+def instantiate_coin(): 
+    #Instantiate the class with user input
+    chosen_coin = choose_coin_to_manage()
+    if chosen_coin not in ["e", "q", ""]:  
+        try:  
+            return currencyScraper(chosen_coin)
+        except: 
+            print("not a valid choice") 
+            control_flow()
+
+    elif chosen_coin == "e": 
+        print(options)
+        control_flow()
+
+    elif chosen_coin == "q": 
+        sys.exit(0) 
+    else: 
+        print("Input is not an option")
+        control_flow()
+
+
+def control_flow(): 
+    # Controls the main control flow of the program (like a tv remote)
+    chosen_coin = instantiate_coin()
+    where = int(input(
+
+f"""
+What do you want to do with {chosen_coin.getName()}\n
+1) Notify me in max and min. 
+2) Get information on coin. 
+3) Get current price
+4) Get URL for complete coin information.\n
+(1, 2, 3, 4) 
+
+====> """))
+
+    # Control flow on where to proceed.
+    if where == 1: 
+        print("Set max and min not complete yet") 
+        print(chosen_coin.set_min_and_max())
+        do_another_task()
+    elif where == 2: 
+        print(chosen_coin)
+        print(chosen_coin.getInfo())
+        do_another_task()
+    elif where == 3: 
+        print(chosen_coin.getPrice())
+        do_another_task()
+    elif where == 4: 
+        print(chosen_coin.getURL())
+        do_another_task()
+    else: 
+        print("Not an option, let's restart") 
+        control_flow()
+
+
+def do_another_task(): 
+    # Dedicated to run after doing something to ask if users needs something else. 
+    next_activity = input("Need something else? (y/n) => ").lower().strip() 
+    if next_activity == "y":
+        control_flow()
+    elif next_activity == "n": 
+        print("Ok, thanks for using.") 
+        sys.exit(0)
+    else: 
+        print("Didn't quite understand input") 
+        do_another_task()
+
+
+# Run main control flow function. 
+control_flow()
+    
+
